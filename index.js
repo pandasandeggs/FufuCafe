fff863f6cc1ded7e79c7a3c019031aa890bdcfb3
pandasandeggs <theteatreehouse@gmail.com>
2020-03-09 13:18:21 -0500

    changed index page and made it responsive

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-function showMenu(name){
-    let menu = document.getElementById(name)
-    console.log("This is the the name passed in : ", typeof(name), name)
-    console.log("Does this match it? ", typeof(menu.id), menu.id)
-    menu === name ? document.getElementById(menu.id).style.display = "block" : document.getElementById(menu.id).style.display = "none";
-}
+
